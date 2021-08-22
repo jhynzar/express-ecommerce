6b54c8de3eb82a66a6acc18786804b7aecd6b846
jhynzar <jhynzar@gmail.com>
2021-08-22 12:13:14 +0800

    [App] Added Database Connection

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // App Entry point
+require('dotenv').config();
+
 const express = require('express');
 
+const databaseConnection = require('./database');
 const initializeRoutes = require('./routes');
 
 const app = express();
 const PORT = 3000;
+
+// Assign Database
+app.set('databaseConnection', databaseConnection);
 
 // Routes
 initializeRoutes(app);
