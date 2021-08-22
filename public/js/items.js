dc99f7a419e72d4e1d7ea7fae621ec7323c513c4
jhynzar <jhynzar@gmail.com>
2021-08-23 05:59:07 +0800

    [Item] Added Create Functionality

--- public/js/items.js
+++ public/js/items.js
@@ -23,6 +23,62 @@ $(document).ready(() => {
 }
 
 /**
+ * Populate Create Modal
+ */
+ function populateCreateModal() {
+    $.ajax({
+        url: '/api/categories',
+        type: 'GET',
+        success: (res) => {
+            if (res.length === 0) {
+                return;
+            }
+            processCreateModalCategoriesHTML(res);
+        }
+    });
+}
+
+/**
+ * Create Item
+ * @param {Array} item {
+ *  name: 'item 1',
+ *  category_ids: [ 1, 2, 3]
+ * }
+ */
+function createItem(item) {
+
+    // Create Item
+    $.ajax({
+        url: '/api/items',
+        type: 'POST',
+        data: { name: item.name },
+        dataType: 'json',
+        success: (res, status) => {
+            if (status === 'success') {
+                const id = res.insertId;
+                // Create Item_Category
+                $.ajax({
+                    url: `/api/items/${id}/categories`,
+                    type: 'POST',
+                    contentType: "application/json; charset=utf-8",
+                    dataType: 'JSON',
+                    data: JSON.stringify({ category_ids: item.category_ids }),
+                    success: (res, status) => {
+                        if (status === 'success') {
+                            confirm('Item Created Successfully');
+                            window.location.reload();
+                        }
+                        
+                        console.log(res);
+                    }
+                });
+            }
+            console.log(res);
+        }
+    });
+}
+
+/**
  * Processors
  */
 
@@ -68,4 +124,39 @@ $(document).ready(() => {
 
     document.querySelector('#itemsTable').innerHTML = itemsMap.join('');
 
+}
+
+/**
+ * Process Create Modal Categories HTML
+ * @param {Array} categories
+ */
+function processCreateModalCategoriesHTML(categories) {
+
+    let categoriesMap = categories.map((category) => {
+        return `
+            <label class="badge bg-success text-white">
+                ${category.name}
+                <input type="checkbox" name="category_ids" value="${category.id}">
+            </label>
+        `;
+    });
+
+    document.querySelector('#createModal .categories').innerHTML = categoriesMap.join('');
+}
+
+/**
+ * Listeners
+*/
+
+/**
+ * Add Item Button onclick Listener
+ */
+ function addItemButtonOnClick() {
+    let itemName = document.querySelector('#createModal input[name=name]').value;
+    let categoryIds = $('#createModal input[name=category_ids]:checked').map(function() { return $(this).val() } ).get();
+
+    createItem({
+        name: itemName,
+        category_ids: categoryIds
+    });
 }
